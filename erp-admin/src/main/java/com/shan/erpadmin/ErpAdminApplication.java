9db5a70a378f6f112ac7edc0de9acc10eb13e6e3
陈俊山 <15813861477@163.com>
2018-05-28 18:17:35 +0800

    提交项目

---
+++ erp-admin/src/main/java/com/shan/erpadmin/ErpAdminApplication.java
@@ -0,0 +1,12 @@
+package com.shan.erpadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpAdminApplication.class, args);
+    }
+}
